ca18507a61b98c07f4a73545fcf7717f45fffb86
Stanley <stanley_shao@126.com>
2026-02-10 19:45:40 +0800

    update IP sorting logic

--- src/database.py
+++ src/database.py
@@ -14,7 +14,7 @@ class Database:
             original_ips = self.file.read().split()
             ips = list(set(ips) | set(original_ips))
             self.file.seek(0)
-        self.file.write('\n'.join(sorted(ips)))
+        self.file.write('\n'.join(sorted(ips, key=lambda ip: tuple(map(int, ip.split('.'))))) + '\n')
 
     def __enter__(self):
         return self
